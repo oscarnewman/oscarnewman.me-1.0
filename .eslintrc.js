0c8e82ab897397220b6d0282e7cd9b7e8b88f65b
Oscar Newman <newman.oscar@gmail.com>
2022-11-23 17:34:30 -0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
   rules: {
+    'react/no-unescaped-entities': 'off',
     'react/self-closing-comp': [
       'error',
       {
